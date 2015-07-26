041cc112a92686d651eaee89e012c42f775eb590
hazrmard <ibragim78786@gmail.com>
2015-07-26 12:58:06 -0500

    Added truncate + consolidate functions
    Improved storage, sorting. Introduced sets for faster consolidation.

--- imgurAPI.py
+++ imgurAPI.py
@@ -21,6 +21,7 @@ class imgurAPI:
         self.worddict = collections.OrderedDict()
         self.isbatch = True
         self.cumulative = True
+        self.tally = collections.OrderedDict()
 
     # use function gallery_item_comments to get comments by item id
 
@@ -44,7 +45,7 @@ class imgurAPI:
         print str(len(self.idlist)) + " items loaded."
         return self
 
-    def filter(self, fname='filter.csv', num=None):           # TODO: import filter list from csv + add option to specify count
+    def filter(self, fname='filter.csv', num=None):
         """read list of words to filter out from analysis"""
         f = open(str(fname), 'rb')
         r = csv.reader(f)
@@ -59,10 +60,15 @@ class imgurAPI:
         f.close()
         if self.cumulative:
             self.worddict = collections.OrderedDict({key: self.worddict[key] for key in self.worddict if key not in filterlist})
+            print "Filter complete."
         else:
+            i = 0
             for itemkey in self.worddict:
                 self.worddict[itemkey] = collections.OrderedDict({key: self.worddict[itemkey][key] for key in self.worddict[itemkey] if key not in filterlist})
-        print "Filter complete."
+                i += 1
+                print "\rFilter " + "{0:.3g}% complete.".format(i*100/len(self.idlist)),
+            self.tally = collections.OrderedDict({key: self.tally[key] for key in self.tally if key not in filterlist})
+            print
         return self
 
     def parse(self, parsechildren=True, cumulative=True):
@@ -85,33 +91,63 @@ class imgurAPI:
         print
         return self
 
+    def consolidate(self):
+        """make dictionary keys uniform"""
+        if not self.cumulative:     # cumulative data needs no consolidation
+            self.tally = collections.OrderedDict(sorted(self.tally.items(), key=lambda t: t[1], reverse=True))  # find most freq words in entire dataset
+            i = 0       # counter
+            datalength = len(self.idlist)
+            tallykeys = set(self.tally.keys())                  # set of all words in all items
+            for itemid in self.idlist:
+                itemkeys = set(self.worddict[itemid].keys())    # set of words in a single item
+                for key in tallykeys - itemkeys:                # iterating over set of words not in item but in tally
+                    self.worddict[itemid][key] = 0              # adding them to item for uniformity
+                i += 1
+                print "\rConsolidating " + "{0:.3g}% complete.".format(i*100/datalength),
+        print
+        return self
+
     def sort(self):
+        """sort words by frequency"""
+        self.tally = collections.OrderedDict(sorted(self.tally.items(), key=lambda t: t[1], reverse=True))
         if self.cumulative:
-            self.worddict = collections.OrderedDict(sorted(self.worddict.items()), key=lambda t: t[1])
+            self.worddict = collections.OrderedDict(sorted(self.worddict.items(), key=lambda t: t[1], reverse=True))
         else:
             for itemid in self.worddict:
-                self.worddict[itemid] = collections.OrderedDict(sorted(self.worddict[itemid].items()), key=lambda t: t[1])
+                self.worddict[itemid] = collections.OrderedDict(sorted(self.worddict[itemid].items(), key=lambda t: t[1], reverse=True))
+        print "Sorting complete."
+        return self
 
-    def store(self, filename='word_dictionary.csv'):                # TODO: implement for cumulative=False
-        tempdict = {}                                               # arrange in order of frequency
+    def store(self, filename='word_dictionary.csv'):
+        """store data to csv file"""
+        f = open(filename, 'wb')                                    # write to csv
+        fr = csv.writer(f)
         if self.cumulative:
-            for key in self.worddict:                                        # reverse word:freq as freq:[word]
-                if self.worddict[key] in tempdict:
-                    tempdict[self.worddict[key]].append(key)
-                else:
-                    tempdict[self.worddict[key]] = [key]
-            templist = [key for key in tempdict]                        # extract list of all freqs
-            templist.sort(reverse=True)                                 # arrange in descending order
-            f = open(filename, 'wb')                                    # write to csv
-            fr = csv.writer(f)
-            for element in templist:                                    # write words in descending order to csv
-                for item in tempdict[element]:
-                    fr.writerow([item, element])
-            f.close()
-            print "Stored in " + filename + "."
+            fr.writerow(self.worddict.keys())
+            fr.writerow([self.worddict[key] for key in self.worddict])
+        else:
+            fr.writerow(['Item ID'] + self.tally.keys())
+            for itemid in self.worddict:
+                fr.writerow([itemid] + [self.worddict[itemid][key] for key in self.tally])
+        f.close()
+        print "Stored in " + filename + "."
+        return self
+
+    def truncate(self, num=None):
+        self.consolidate()
+        if num is not None:
+            keys = self.tally.keys()[:num]      # truncate key list
+            self.tally = collections.OrderedDict({key:self.tally[key] for key in keys})     # update tally
+        if self.cumulative:
+            self.worddict = collections.OrderedDict({key: self.worddict[key] for key in self.tally})
+        else:
+            for itemid in self.worddict:
+                self.worddict[itemid] = collections.OrderedDict({key: self.worddict[itemid][key] for key in self.tally})
+        print "Truncated to top " + str(num) + " words."
         return self
 
     def credits(self):
+        """check usage with imgur API"""
         print "\nCredits:"
         print str(self.client.credits['UserRemaining']) + " remaining (IP based) out of " + str(self.client.credits['UserLimit'])
         print str(self.client.credits['ClientRemaining']) + " remaining (API key based) out of " + str(self.client.credits['ClientLimit'])
@@ -133,6 +169,10 @@ class imgurAPI:
                         storage[w] += 1
                     else:
                         storage[w] = 1
+                    if w in self.tally:     # to keep a cumulative tally of words for truncation and storing purposes
+                        self.tally[w] += 1
+                    else:
+                        self.tally[w] = 1
                 except:
                     continue
         else:
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ client_secret = '***REMOVED***'
 
 imgurInstance = imgurAPI.imgurAPI(client_id, client_secret)
 
-imgurInstance.get().parse(parsechildren=False).filter(num=1000).store().credits()
+imgurInstance.get().parse(parsechildren=False, cumulative=False).filter(num=1000).truncate(1000).sort().store().credits()
 
